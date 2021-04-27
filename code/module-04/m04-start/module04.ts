b942822daa3067ae9e4ef878b2d758c6e02530ca
Christopher Harrison <chrhar@microsoft.com>
2021-04-27 13:36:56 -0700

    quick updates

--- code/module-04/m04-start/module04.ts
+++ code/module-04/m04-start/module04.ts
@@ -12,9 +12,9 @@
     numbers in descending order */
 function sortDescending(a, b) {
 if (a > b) {
-    return -1;;
+    return -1;
 } else if (b > a) {
-    return 1;;
+    return 1;
 } else {
     return 0;
 }
